b52d8812f0f0c16223058e8df406b147267ef13c
gradiuscypher <gradiuscypher@gmail.com>
2017-08-06 21:18:44 -0700

    adding windows connection info

--- net/net_windows.go
+++ net/net_windows.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/shirou/gopsutil/internal/common"
 	"golang.org/x/sys/windows"
+	"os/exec"
+	"strings"
+	"strconv"
 )
 
 var (
@@ -71,9 +74,100 @@ func IOCountersByFile(pernic bool, filename string) ([]IOCountersStat, error) {
 
 // Return a list of network connections opened by a process
 func Connections(kind string) ([]ConnectionStat, error) {
+	connections, err := ParseNetstat()
+	return connections, err
+}
+
+// Return a list of network connections opened by a process.
+func ConnectionsPid(kind string, pid int32) ([]ConnectionStat, error) {
+	//ParseNetstat
+	//Return List of ConnectionStat that match pid
+	return []ConnectionStat{}, common.ErrNotImplementedError
+}
+
+//Parse netstat output into a collection of connections
+func ParseNetstat() ([]ConnectionStat, error){
 	var ret []ConnectionStat
 
-	return ret, common.ErrNotImplementedError
+	//Generate Netstat TCP Output
+	tcp_out, err := exec.Command("cmd", "/c", "netstat -anop tcp").Output()
+	if err == nil {
+		//Iterate through netstat text and create ConnectionStat objects
+		lines := strings.Split(string(tcp_out), "\n")
+
+		for _, line := range lines {
+			split := strings.Fields(line)
+			if len(split) > 0 {
+				if split[0] == "TCP" {
+					//protocol := split[0]
+					local_str := split[1]
+					remote_str := split[2]
+					state := split[3]
+					pid,_ := strconv.Atoi(split[4])
+					local_address, _ := ParseAddress(local_str)
+					remote_address, _:= ParseAddress(remote_str)
+
+					conn := ConnectionStat{
+						Fd:     0,
+						Family: 0,
+						Type:   0,
+						Laddr:  local_address,
+						Raddr:  remote_address,
+						Status: state,
+						Pid:    int32(pid),
+					}
+					ret = append(ret, conn)
+				}
+			}
+		}
+	}
+
+	//Generate Netstat UDP Output
+	udp_out, err := exec.Command("cmd", "/c", "netstat -anop udp").Output()
+	if err == nil {
+		//Iterate through netstat text and create ConnectionStat objects
+		lines := strings.Split(string(udp_out), "\n")
+
+		for _, line := range lines {
+			split := strings.Fields(line)
+			if len(split) > 0 {
+				if split[0] == "UDP" {
+					local_str := split[1]
+					pid,_ := strconv.Atoi(split[3])
+					local_address, _ := ParseAddress(local_str)
+
+					//FIXME: UDP doesn't have remote addresses
+					//FIXME: UDP doesn't have status
+					remote_address, _:= ParseAddress("0.0.0.0:0")
+
+
+					conn := ConnectionStat{
+						Fd:     0,
+						Family: 0,
+						Type:   0,
+						Laddr:  local_address,
+						Raddr:  remote_address,
+						Status: "TIME_WAIT",
+						Pid:    int32(pid),
+					}
+					ret = append(ret, conn)
+				}
+			}
+		}
+	}
+	//Return list of ConnectionStat objects
+	return ret, nil
+}
+
+//Parse addresses from netstat segment
+func ParseAddress(addr_str string) (Addr, error){
+	split_addr := strings.Split(addr_str, ":")
+	ip := split_addr[0]
+	port,_ := strconv.Atoi(split_addr[1])
+	return Addr{
+		IP:   ip,
+		Port: uint32(port),
+	}, nil
 }
 
 // Return a list of network connections opened returning at most `max`
